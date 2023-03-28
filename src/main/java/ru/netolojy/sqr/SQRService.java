5e7cdb3147934328fa7117944a5c0e16fc1c8019
Helen <nightcorvux24@gmail.com>
2023-03-29 03:46:23 +0700

    first commit

---
+++ src/main/java/ru/netolojy/sqr/SQRService.java
@@ -0,0 +1,13 @@
+package ru.netolojy.sqr;
+public class SQRService {
+    public int TestNumber(int min, int max) {
+        int number = 0;
+        for (int i = 10; i <= 99; i++) {
+            int x = i * i;
+            if (x >= min && x <= max) {
+                number++;
+            }
+        }
+        return number;
+    }
+}
